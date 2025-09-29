235cff693aa68e26edeab952ebd61e69ece1a1c5
gleb <demkingleb5@gmail.com>
2025-09-29 19:01:38 +0300

    Add files via upload

---
+++ gleb_code/project_gleb.py
@@ -0,0 +1,23 @@
+'''class User: # плюс наследование
+    def __init__(self, user_id, role, email, password):
+    def get(self, id):
+    def post(self, role, email, password):
+    def edit(self, role, email, password):
+    def delete(self, id):
+    def get_all(self):
+        
+class User_Course:
+    def __init__(self, id, course_id, user_id):
+        
+class Course:
+    def __init__(self, id, name, chapter):
+        
+class Chapter:
+    def __init__(self, id, lesson, task):
+        
+class Task:
+    
+class Lesson:
+    
+class Rating:
+    def __init__(self, id, user_id, task_id, score):'''
